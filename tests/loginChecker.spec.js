778ecd3b7901b2ad32636954d9ace9fe923298d8
MeanFishy00 <Isaiah.greenwood01@gmail.com>
2024-11-18 12:25:39 -0600

    Added flow for locked user
    Added workflow for locked user and will locate error message and display it

--- tests/loginChecker.spec.js
+++ tests/loginChecker.spec.js
@@ -6,6 +6,7 @@
  * to the inventory page.
  */
 const { test, expect } = require("@playwright/test");
+const { error } = require("console");
 
 test("login checker", async ({ page }) => {
   // Navigate to the Sauce Demo login page
@@ -20,4 +21,25 @@ test("login checker", async ({ page }) => {
 
   // Verify successful login by checking if URL matches the inventory page
   await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+});
+
+test("locked user", async ({ page }) => {
+  // Navigate to the Sauce Demo login page
+  await page.goto("https://www.saucedemo.com/");
+
+  // Fill in login credentials using placeholder text to locate elements
+  await page.getByPlaceholder("Username").fill("locked_out_user");
+  await page.getByPlaceholder("Password").fill("secret_sauce");
+
+  // Click the login button using accessibility role selector
+  await page.getByRole("button", { name: "Login" }).click();
+
+  // Looks for error message to appear
+  let err = (await page.locator('[data-test="error"]').innerText());
+
+  // Prints the Error message
+  console.log("Error Message: " + err)
+
+  // Verify successful login by checking if URL matches the inventory page
+  await expect(page).toHaveURL("https://www.saucedemo.com/");
 });
